f769cb85abbac7a8ae0604edb808a22562d93fbe
tomgiagtz <tom.giagtzoglou@gmail.com>
2019-11-18 23:19:53 -0500

    add force arrow

--- Ball.js
+++ Ball.js
@@ -5,8 +5,12 @@ class Ball {
     this.r = r;
     this.angle = 0;
     this.angV = 0;
+    this.mag = 1;
   }
 
+  setMagnitude(mag) {
+    this.mag = mag;
+  }
   setAngularVelocity(angV) {
     this.angV = angV;
   }
@@ -25,5 +29,40 @@ class Ball {
     arc(this.x, this.y, this.r, this.r, PI, 0);
     fill(150);
     arc(this.x, this.y, this.r, this.r, 0, PI);
+  }
+
+  showForce() {
+    const up = this.mag >= 0 ? 1 : -1;
+    const width = (this.mag * sqrt(10)) / 2;
+
+    beginShape();
+    stroke(0);
+    strokeWeight(1.1);
+    fill(237, 34, 93);
+    //origin
+    vertex(this.x, this.y);
+    //bottom left of rect
+    vertex(this.x - width, this.y);
+    //top left of rect
+    vertex(this.x - width, this.y - width * this.mag);
+    //left triangle tip
+    vertex(this.x - width * 1.5, this.y - width * this.mag);
+    //top triangle tip
+    vertex(this.x, this.y - width * this.mag - up * 2 * width);
+    vertex(this.x + width * 1.5, this.y - width * this.mag);
+    vertex(this.x + width, this.y - width * this.mag);
+    vertex(this.x + width, this.y);
+    //origin
+    vertex(this.x, this.y);
+    endShape(CLOSE);
+
+    // line(this.x, this.y, this.x - width, this.y);
+    // line(this.x - width, this.y, this.x - width, this.y - width * mag);
+    // line(this.x - width, this.y - width * mag, this.x - width * 1.5, this.y - width * mag);
+    // line(this.x - width * 1.5, this.y - width * mag, this.x, this.y - width * mag - 2 * width);
+    // line(this.x + width * 1.5, this.y - width * mag, this.x, this.y - width * mag - 2 * width);
+    // line(this.x + width, this.y - width * mag, this.x + width * 1.5, this.y - width * mag);
+    // line(this.x + width, this.y, this.x + width, this.y - width * mag);
+    // line(this.x, this.y, this.x + width, this.y);
   }
 }
--- sketch.js
+++ sketch.js
@@ -1,18 +1,32 @@
-let ball = new Ball(200, 200, 50);
+let ball;
 let angularVelocitySlider;
+let forceCheck;
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  ball = new Ball(windowWidth / 2, windowHeight / 2, 125);
+  textSize(15);
   background(0);
   angularVelocitySlider = createSlider(0.005, PI, 0.01, 0.001);
-  angularVelocitySlider.position(windowWidth - 100, 10);
+  fill(255);
+  text("Angular Velocity", windowWidth * 0.75 - windowWidth * 0.075, 20);
+  noFill();
+  angularVelocitySlider.position(windowWidth * 0.75, 20);
   angularVelocitySlider.style("width", "80px");
+  forceCheck = createCheckbox("Show Force", false);
+  forceCheck.position(windowWidth * 0.75, 40);
+  forceCheck.style("color", "#DDD");
+  forceCheck.changed(() => background(0));
   stroke(255);
 }
-
+let mag = 0.001;
 function draw() {
+  mag += 0.01;
+  ball.setMagnitude(mag);
   ball.setAngularVelocity(angularVelocitySlider.value());
-  line(100, 100, 50, 0);
-  ellipse(100, 100, 50);
 
   ball.spin();
+
+  if (forceCheck.checked()) {
+    ball.showForce();
+  }
 }
